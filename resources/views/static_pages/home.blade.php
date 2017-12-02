9cb6a9f2c106feee962058d6d38fe38c19a3b79b
vagrant <vagrant@homestead>
2017-12-02 14:27:28 +0000

    update

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,16 @@
 @section('title','主页')
 
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是Laravel
+        </p>
+        <p>
+            一切从此开始
+        </p>
+        <p>
+            <a href="{{ route('signup') }}" role="button" class="btn btn-lg btn-success">现在注册</a>
+        </p>
+    </div>
 @stop
